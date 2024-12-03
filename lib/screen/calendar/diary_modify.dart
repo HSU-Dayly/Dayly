43a5f98584fe3854bfd07a0c7da3afd470a4b177
니요리 <138342064+niroey@users.noreply.github.com>
2024-12-04 00:05:23 +0900

    [yr]수정요

--- lib/screen/calendar/diary_modify.dart
+++ lib/screen/calendar/diary_modify.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'diary_list.dart';
 import '../diary/DiarySwipeScreen.dart';
+import 'diary_list.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; // Firestore import
 
 class DiaryModifyScreen extends StatelessWidget {
   final DateTime date;
@@ -46,7 +47,6 @@ class DiaryModifyScreen extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                // 날짜는 맨 왼쪽에 위치
                 Text(
                   formattedDate,
                   style: TextStyle(
@@ -54,15 +54,26 @@ class DiaryModifyScreen extends StatelessWidget {
                     color: Color.fromRGBO(88, 71, 51, 0.592),
                   ),
                 ),
-                // 수정/삭제 버튼은 맨 오른쪽에 위치
                 Row(
                   children: [
                     Container(
                       color: Colors.yellow[100],
                       child: TextButton(
                         onPressed: () {
-                          // 수정 버튼 동작
-                          // 일기 작성 화면
+                          // 수정 화면으로 이동
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //     builder: (context) => DiarySwipeScreen(
+                          //       date: date,
+                          //       initialContent: content,
+                          //       onSave: (updatedContent) {
+                          //         // 수정된 내용 저장 후 돌아오기
+                          //         // Firestore 업데이트 로직도 여기에 추가 가능합니다.
+                          //       },
+                          //     ),
+                          //   ),
+                          // );
                         },
                         style: TextButton.styleFrom(
                           padding: EdgeInsets.zero,
@@ -71,10 +82,7 @@ class DiaryModifyScreen extends StatelessWidget {
                         ),
                         child: const Text(
                           '수정',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,
-                          ),
+                          style: TextStyle(fontSize: 15, color: Colors.black),
                         ),
                       ),
                     ),
@@ -83,7 +91,6 @@ class DiaryModifyScreen extends StatelessWidget {
                       color: Colors.yellow[100],
                       child: TextButton(
                         onPressed: () {
-                          // 삭제 버튼 동작
                           _showDeleteDialog(context);
                         },
                         style: TextButton.styleFrom(
@@ -93,10 +100,7 @@ class DiaryModifyScreen extends StatelessWidget {
                         ),
                         child: const Text(
                           '삭제',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,
-                          ),
+                          style: TextStyle(fontSize: 15, color: Colors.black),
                         ),
                       ),
                     ),
@@ -118,37 +122,18 @@ class DiaryModifyScreen extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: SizedBox(
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: List.generate(4, (index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.popUntil(context, (route) => route.isFirst);
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.width / 4,
-                height: 60,
-                color: Colors.transparent,
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: _getIcon(index),
-              ),
-            );
-          }),
-        ),
-      ),
     );
   }
 
-  // 삭제 확인 다이얼로그 함수 추가
   void _showDeleteDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext dialogContext) {
         return AlertDialog(
-          title: const Text('일기를 삭제하시겠습니까? 삭제한 일기 내용은 복구할 수 없습니다.',
-              style: TextStyle(fontSize: 17)),
+          title: const Text(
+            '일기를 삭제하시겠습니까? 삭제한 일기 내용은 복구할 수 없습니다.',
+            style: TextStyle(fontSize: 17),
+          ),
           actions: [
             TextButton(
               child: const Text('취소'),
@@ -159,9 +144,10 @@ class DiaryModifyScreen extends StatelessWidget {
             TextButton(
               child: const Text('삭제'),
               onPressed: () {
-                // 일기 삭제 로직 실행
                 Navigator.pop(dialogContext); // 다이얼로그 닫기
-                onDelete(date); // 삭제 처리 후 뒤로 가기
+                _deleteDiaryFromFirestore(context, date); // Firestore에서 삭제
+                onDelete(date); // 부모에서 전달된 삭제 함수 호출
+                Navigator.pop(context); // 현재 화면 닫고 이전 화면으로 돌아가기
               },
             ),
           ],
@@ -170,51 +156,34 @@ class DiaryModifyScreen extends StatelessWidget {
     );
   }
 
-// DiaryListScreen의 diaryEntries에서 해당 날짜 일기 삭제
-  void _deleteDiaryEntry(DateTime date, BuildContext context) {
-    DiaryListScreen.diaryEntries.removeWhere(
-      (entry) => entry['date'] == date,
-    );
-    Navigator.pop(context); // 이전 화면으로 이동
-  }
+  // Firestore에서 일기 삭제하는 함수
+  Future<void> _deleteDiaryFromFirestore(
+      BuildContext context, DateTime date) async {
+    try {
+      // 날짜 형식 지정 (문서 ID로 사용)
+      String formattedDate = DateFormat('yyyy-MM-dd').format(date);
 
-  Widget _getIcon(int index) {
-    switch (index) {
-      case 0:
-        return Image.asset(
-          'assets/images/home.png',
-          width: 25,
-          height: 25,
-          color: const Color.fromRGBO(88, 71, 51, 0.992),
-        );
-      case 1:
-        return Image.asset(
-          'assets/images/dictionary.png',
-          width: 25,
-          height: 25,
-          color: const Color.fromRGBO(88, 71, 51, 0.992),
-        );
-      case 2:
-        return Image.asset(
-          'assets/images/words.png',
-          width: 25,
-          height: 25,
-          color: const Color.fromRGBO(88, 71, 51, 0.992),
-        );
-      case 3:
-        return Image.asset(
-          'assets/images/user.png',
-          width: 25,
-          height: 25,
-          color: const Color.fromRGBO(88, 71, 51, 0.992),
-        );
-      default:
-        return Image.asset(
-          'assets/images/home.png',
-          width: 25,
-          height: 25,
-          color: const Color.fromRGBO(88, 71, 51, 0.992),
-        );
+      // Firestore 컬렉션 참조
+      CollectionReference diaries =
+          FirebaseFirestore.instance.collection('diaries');
+
+      // 해당 문서 삭제
+      await diaries.doc(formattedDate).delete();
+
+      // 삭제 성공 메시지 표시
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('일기가 성공적으로 삭제되었습니다.'),
+        ),
+      );
+    } catch (e) {
+      // 삭제 실패 시 에러 처리
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('일기 삭제에 실패했습니다.'),
+        ),
+      );
+      print("Error deleting diary: $e");
     }
   }
 }
